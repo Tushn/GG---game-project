1e307a0c1cdf0b95128fe175ee5465bbf34731b4
Tushin <artur.fhtagn@gmail.com>
2024-10-13 15:51:10 -0300

    Pequena atualização para teste e organização do projeto

---
+++ js/map.js
@@ -0,0 +1,203 @@
+// Map and Tiles
+var Tile = function(dy, dx, dims, img, context){
+	this.dx = dx;
+	this.dy = dy;
+	this.sx = dims[0];
+	this.sy = dims[1];
+	// variables of passability
+	this.passability = dims[2];
+	this.height = dims[3];
+	
+	this.img = img;
+	this.context = context;
+	var _type = [0,0]; // passability, height
+	
+	// Getters and setters	
+	this.getType = function(type){ 
+		if(type==undefined) 
+			return _type; 
+		else 
+			return _type[type]; 
+	}
+	this.setType = function(type, value){
+		if(value==undefined) value = 1;			
+		_type[type] = value;
+	}
+	
+	// Draw tile
+	this.draw = function(cx, cy){
+		this.context.drawImage(img, this.sx*TILE_WIDTH, this.sy*TILE_WIDTH, TILE_WIDTH, TILE_HEIGHT, this.dx-cx, this.dy-cy, TILE_WIDTH, TILE_HEIGHT);
+	}
+}
+
+// Map
+var Map = function(src1, src2, map1, map2, context, camera){
+	this.tileMap = [new Array(),new Array()];
+	var img_level_1 = new Image();
+	var img_level_2 = new Image();
+	this.context = context;
+	this.entities = new Array(); // a primeira entidade é o jogador, ou seja, this.entities[0]
+	this.camera = camera; if(camera==undefined){ this.camera={}; this.camera.x = 0; this.camera.y = 0; };
+	this.player;
+	
+	Map.width = TILE_WIDTH*map1[0].length;
+	Map.height = TILE_HEIGHT*map1.length;
+	
+	img_level_1.src = "img/" + src1; // imagem com url (image) do Character
+	img_level_2.src = "img/" + src2; // imagem com url (image) do Character
+	
+	for(var i=0;i<map1.length;i++){
+		this.tileMap[0][i] = new Array();
+		this.tileMap[1][i] = new Array();
+		for(var j=0;j<map1[i].length;j++){
+			this.tileMap[0][i][j] = new Tile(i*TILE_WIDTH, j*TILE_HEIGHT, map1[i][j], img_level_1, this.context);
+			this.tileMap[1][i][j] = new Tile(i*TILE_WIDTH, j*TILE_HEIGHT, map2[i][j], img_level_2, this.context);
+		}
+	}
+		
+	img_level_1.onload = function(){
+		Image.spritesLoadedCount++;
+		if(Image.spritesToLoad<=Image.spritesLoadedCount){
+			gameStatus = GAME_INITING;
+		}else
+			gameStatus = GAME_LOADING;
+	}
+
+	img_level_2.onload = img_level_1.onload;
+	
+	
+	/*******************************
+	*********** METHODS ************
+	********************************/
+	// GETTERS AND SETTERS
+	// add new entity in array
+	this.addEntity = function(entity){
+		this.entities.push(entity);
+		entity.map = this;
+		entity.camera = this.camera;
+	}
+	this.makeEntity = function(type, image, x, y, width, height, divsWidth, divsHeight, context, name){
+		this.addEntity(FactoryEntity.make(type, image, x, y, width, height, divsWidth, divsHeight, context, name));
+	}
+	this.reuseEntity = function(id, changed){
+		this.addEntity(FactoryEntity.reuse(this.entities[id], changed));
+	}
+	this.reuseRandomPosition = function(times, tileNumX, tileNumY, id){
+		id = (id==undefined)? this.entities.length-1 : id;
+		var objs = FactoryEntity.reuseRandomPosition(this.entities[id], times, tileNumX, tileNumY);
+		for(var i in objs) this.addEntity(objs[i]);
+	}
+	this.reuseByNameRandomPosition = function(times, tileNumX, tileNumY, name){
+		var text = "", cont = -1;
+		while(text!=name && cont < this.entities.length){
+			text = this.entities[++cont].name;
+		}
+		if(text!=name) throw new Error("Object with name '"+name+"' is not found");
+		var objs = FactoryEntity.reuseRandomPosition(this.entities[cont], times, tileNumX, tileNumY);
+		
+		for(var i=0;i<times;i++) this.addEntity(objs[i]);
+	}
+	// add player in player reference and entity
+	this.addPlayer = function(player){
+		this.addEntity(player);
+		this.player = player;
+	}
+	
+	// set reference of entity each others
+	this.setEntitiesRefs = function(){
+		for(var i=0;i<this.entities.length;i++)
+			for(var j=i+1;j<this.entities.length;j++){
+				this.entities[i].entities.push(this.entities[j]);
+				this.entities[j].entities.push(this.entities[i]);
+			}
+	}
+
+	// UPDATE AND DRAWS
+	// update camera for player
+	this.updateCamera = function(){
+		this.camera.x = this.player.x - canvas.width/2;
+		this.camera.y = this.player.y - canvas.height/2;
+		
+		if(this.camera.x<0)
+		this.camera.x = 0;
+
+		if(this.camera.y<0)
+		this.camera.y = 0;
+	
+		if(this.camera.x+canvas.width>(this.tileMap[0][0].length-1)*TILE_WIDTH)
+			this.camera.x=(this.tileMap[0][0].length)*TILE_WIDTH-canvas.width;
+		if(this.camera.y+canvas.height>(this.tileMap[0].length-1)*TILE_HEIGHT)
+			this.camera.y=(this.tileMap[0].length)*TILE_HEIGHT-canvas.height;
+	}
+	
+	// draw tiles
+	this.draw = function(){
+		// draw all tiles
+		for(var i=0;i<this.tileMap[0].length;i++){
+			for(var j=0;j<this.tileMap[0][i].length;j++){
+				this.tileMap[0][i][j].draw(this.camera.x,this.camera.y);
+				this.tileMap[1][i][j].draw(this.camera.x,this.camera.y);
+			}
+		}
+	}
+	
+	// sort entities
+	this.sortEntities = function(){
+		var temp;
+		for(var i=1;i<this.entities.length;i++)
+			if(this.entities[i-1].y+this.entities[i-1].height>this.entities[i].y+this.entities[i].height){
+				temp = this.entities[i-1];
+				this.entities[i-1] = this.entities[i];
+				this.entities[i] = temp;
+			}
+	}
+
+	//  draw tile and camera
+	this.update = function(){
+		if(gameStatus!=GAME_PAUSE){
+			this.draw(this.camera);
+			this.updateCamera();
+			
+			this.sortEntities();
+			// update entity
+			for(var i=0;i<this.entities.length;i++)
+				this.entities[i].update(this.camera); // update
+		}
+	}
+}
+
+// statics variables for dimensions, they are updating on constructor
+Map.width = 320;
+Map.height = 320;
+
+var FactoryEntity = function(){}
+FactoryEntity.make = function(type, image, x, y, width, height, divsWidth, divsHeight, context, name){
+	switch(type.toUpperCase()){
+		case "ITEM":
+			return new Item(image, x, y, width, height, divsWidth, divsHeight, context, name);
+			break;
+		case "CHARACTER":
+			return new Character(image, x, y, width, height, divsWidth, divsHeight, context, name);
+			break;
+		case "NPC":
+			return new NPC(image, x, y, width, height, divsWidth, divsHeight, context, name);
+			break;
+	}
+}
+FactoryEntity.reuse = function(object, changed){
+	var obj = object.clone();
+	if(changed!=undefined)
+		for(var i in changed)
+			obj[i] = changed[i];
+	return obj;
+}
+FactoryEntity.reuseRandomPosition = function(object, times, tileNumX, tileNumY){
+	var obj = [];
+	for(var i=0;i<times;i++){
+		obj.push(object.clone());
+		// obj.image = object.image.cloneNode();
+		obj[obj.length-1].x = (Math.randi(tileNumX)+1)*TILE_WIDTH;
+		obj[obj.length-1].y = (Math.randi(tileNumY)+1)*TILE_HEIGHT;
+	}
+	return obj;
+}
---
+++ js/utils.js
@@ -0,0 +1,179 @@
+// Constantes e variaveis globais
+var canvas = document.getElementById("my_sample");
+var context = canvas.getContext("2d");
+context.font = "italic bold 30px serif";
+var game = {}; // loaded in 'load.js'
+var keypressed;
+var gameStatus = 0;
+var player, enemies = {}; enemies.points = 0;
+
+// Constantes
+// const TIME_UPDATE = 35;
+const TIME_UPDATE = 135;
+const GAME_RUNNING = 0;
+const GAME_LOADING = 1;
+const GAME_INITING = 2;
+const GAME_INIT = 20;
+const GAME_PAUSE = 155;
+const VELOCITY = 10;
+
+const TILE_WIDTH = 32;
+const TILE_HEIGHT = 32;
+
+// keyboard
+const LEFT = 37;
+const UP = 38;
+const RIGHT = 39;
+const DOWN = 40;
+const ENTER = 13;
+
+// mouse control
+var element_over;
+
+// tiles
+var TILES = Object.freeze({PASSABILITY: 0, HEIGHT:1 }); // this is enumarete of JS
+
+my_sample.onclick = function(){
+	game.clicked();
+}
+// Static variable for load Images
+Image.spritesLoadedCount = 0;
+Image.spritesToLoad = 0;
+
+// Gera valor inteiro no intervalo I = [0,n]
+Math.randi = function(n){
+	return Math.round(Math.random()*(n+1));
+}
+
+/* -----------------------------------------------
+ -------------------- SPRITE ---------------------
+ ----------------------------------------------- */
+var Sprite = function(image, x, y, width, height, divsWidth, divsHeight, context, camera){
+	this.image = new Image();	this.image.src = "img/" + image; // imagem com url (image) do Character
+	Image.spritesToLoad++;
+	
+	this.divsWidth = divsWidth; this.divsHeight = divsHeight;
+	this.width = width;	this.height = height; // altura e largura do Character
+	this.sw; this.sh;
+	this.camera = camera;
+	if(this.camera===undefined){
+		this.camera = {};
+		this.camera.x = 0;
+		this.camera.y = 0;
+	}
+	this.timeWorldO = Date.now(); // tempo global
+	this.timeUpdateConst = TIME_UPDATE; // atualizacao do frame
+	
+	this.x = x;	this.y = y; // posicao x e y do Character no mundo
+	this.context = context;
+	this.direction = 0; // 0-down, 1-left, 2-up, 3-right
+	this.slide = 0;
+	this.slide_limit=4; // padrao
+	this.walking = 0;
+	
+	// escrita do metodo de load da imagem
+	this.image.onload = function(){
+		Image.spritesLoadedCount++;
+		if(Image.spritesToLoad<=Image.spritesLoadedCount){
+			gameStatus = GAME_INIT;
+		}else
+			gameStatus = GAME_LOADING;
+	}
+	// funcao que controla a atulizacao de frames, ela retorna true a cada atualizacao
+	this.timeUpdate = function(){
+		if(this.timeWorldO+this.timeUpdateConst<Date.now()){
+			this.timeWorldO=Date.now();  // quando atualizar execute acao
+			return true;
+		}
+		return false;
+	}
+	// desenho e controle da animacao
+	this.draw = function(){
+		// Controle dos slides da animacao
+		if(this.walking>0 && this.slide < this.slide_limit-1){
+			if(this.timeUpdate()){
+				this.slide=this.slide+1;
+			}
+		}else
+			this.slide=0;
+		
+		this.context.drawImage(this.image, 
+		(this.image.width/this.divsWidth)*this.slide, (this.image.height/this.divsHeight)*this.direction, 
+		(this.image.width/this.divsWidth), (this.image.height/this.divsHeight),
+		this.x-this.camera.x, this.y-this.camera.y ,
+		this.width, this.height);
+	}
+	// for other animations
+	this.update = function(){
+		this.draw();
+	}
+}
+
+/* -----------------------------------------------
+ --------------------- FONT ----------------------
+ ----------------------------------------------- */
+var Font = function(font, x, y, tam, context){
+	this.font = font;
+	this.x = x;	 this.y = y;
+	this.tam = tam+"px";
+
+	this.context = context;
+
+	this.width = 0;
+	this.height = 0;
+
+	this.draw = function(){
+		this.context.fillText((this.tam+" "+this.font), this.x, this.y);
+	}
+ }
+/* -----------------------------------------------
+ ------------------- COLLISION -------------------
+ ----------------------------------------------- */
+var Collision = function(x,y,width,height){
+	this.cx = x;
+	this.cy = y;
+	this.cwidth = width;
+	this.cheight = height;
+	
+	// getters
+	this.collisionY = function(){	return this.y + this.cy; }
+	this.collisionX = function(){ return this.x + this.cx; }
+	this.cHeight = function(){ return this.collisionY()+this.cheight; }
+	this.cWidth = function(){ return this.collisionX()+this.cwidth; }
+	// verify collisions
+	this.collisionEntity = function(entity){
+		var col_y=false, result = false;
+		// if this obj is than high position and its base is less high than other
+		if(this.collisionY()<entity.collisionY()){
+			if(this.collisionY()+this.cheight>entity.collisionY())
+				col_y = true;
+		}else{ // if this obj is more high 
+			if(this.collisionY()<entity.collisionY()+entity.cheight)
+				col_y = true;
+		}
+		if(col_y){
+			if(this.collisionX()<entity.collisionX()){
+				if(this.collisionX()+this.cwidth>entity.collisionX())
+					result = true;
+			}else{
+				if(this.collisionX()<entity.collisionX()+entity.cwidth)
+					result = true;
+			}
+		}
+		if(result){
+			this.collision_event(entity);
+			entity.collision_event(this);
+			return true;
+		}
+		return false;
+	}
+	
+	this.collisionDebug = function(context){
+		context.fillRect(this.cx, this.cy, this.cwidth, this.cheight);
+	}
+	// metodo abstrato
+	this.collisionEvent = function(entity){
+		// implemente-o
+	}
+}
+
